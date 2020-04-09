f565edd2149a002c03d0f459c1457bc57b915542
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-09 16:19:03 -0400

    Basic HUD

---
+++ Assets/Normcore/Scripts/SceneTransitions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class SceneTransitions : MonoBehaviour
+{
+
+    public TMP_Dropdown sceneSelector;
+
+    public void RouteToBowl()
+    {
+      if(SceneManager.GetActiveScene().name == "Pinning Scene") SceneManager.LoadScene("Bowl Scene");
+    }
+
+    public void RouteToCircle()
+    {
+      if(SceneManager.GetActiveScene().name == "Bowl Scene") SceneManager.LoadScene("Pinning Scene");
+    }
+
+    public void Route()
+    {
+      if(sceneSelector.value == 0) RouteToBowl();
+      if(sceneSelector.value == 1) RouteToCircle();
+    }
+}
--- Assets/Normcore/Scripts/ThirdPersonPlayer.cs
+++ Assets/Normcore/Scripts/ThirdPersonPlayer.cs
@@ -22,15 +22,7 @@ using Normal.Realtime;
             // Make sure we own the transform so that RealtimeTransform knows to use this client's transform to synchronize remote clients.
             _realtimeTransform.RequestOwnership();
 
-            // Grab the x/y input from WASD / a controller
-            // float x = Input.GetAxis("Horizontal");
-            // float y = Input.GetAxis("Vertical");
-
-            // // Apply to the transform
-            // Vector3 localPosition = transform.localPosition;
-            // localPosition.x += x * speed * Time.deltaTime;
-            // localPosition.y += y * speed * Time.deltaTime;
-            // transform.localPosition = localPosition;
+            // GUI Here
         }
     }
 // }
