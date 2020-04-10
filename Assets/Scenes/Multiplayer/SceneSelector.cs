4ae2156edd847c8cb02cd04b27dfdf07d4482599
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-10 01:29:40 -0400

    Scene Selection HUD

---
+++ Assets/Scenes/Multiplayer/SceneSelector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// using UnityEngine.UI;
+// using TMPro;
+
+public class SceneSelector : MonoBehaviour
+{
+
+    // public TMP_Dropdown sceneSelector;
+
+    public void LoadCircle()
+    {
+      if(SceneManager.GetActiveScene().name == "Bowl Scene") SceneManager.LoadScene("Pinning Scene");
+    }
+
+    public void LoadBowl()
+    {
+      if(SceneManager.GetActiveScene().name == "Pinning Scene") SceneManager.LoadScene("Bowl Scene");
+    }
+
+    // public void SelectScene()
+    // {
+    //   if(SceneManager.GetActiveScene().name != sceneSelector.options[sceneSelector.value].text) SceneManager.LoadScene(sceneSelector.options[sceneSelector.value].text);
+    // }
+}
