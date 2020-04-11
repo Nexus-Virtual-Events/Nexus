a55004c83edff70818442269d2f39eb85a922b06
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-11 01:04:26 -0400

    NameSync

--- Assets/Scenes/Multiplayer/Main Menu/MainMenu.cs
+++ Assets/Scenes/Multiplayer/Main Menu/MainMenu.cs
@@ -12,21 +12,9 @@ namespace Michsky.UI.ModernUIPack
       public TMP_InputField playerName;
       public string scene;
 
-      // Start is called before the first frame update
-      void Start()
-      {
-
-      }
-
-      // Update is called once per frame
-      void Update()
-      {
-
-      }
-
       public void StartGame()
       {
-        PlayerName.playerName = playerName.text;
+        PlayerPrefs.SetString("playerName", playerName.text);
         Loading.sceneString = scene;
         SceneManager.LoadScene("Loading");
       }
