4080dbe03b580c8e22c9c0a684d523917056c7c4
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-11 14:22:16 -0400

    HDRP NameSync

---
+++ Assets/Normal/Examples/Cube Player/Scripts/CubePlayer.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using Normal.Realtime;
+
+namespace Normal.Realtime.Examples {
+    public class CubePlayer : MonoBehaviour {
+      public float speed = 5.0f;
+
+      private RealtimeView      _realtimeView;
+      private RealtimeTransform _realtimeTransform;
+
+      /*
+      Writen by Windexglow 11-13-10.  Use it, edit it, steal it I don't care.
+      Converted to C# 27-02-13 - no credit wanted.
+      Simple flycam I made, since I couldn't find any others made public.
+      Made simple to use (drag and drop, done) for regular keyboard layout
+      wasd : basic movement
+      shift : Makes camera accelerate
+      space : Moves camera on X and Z axis only.  So camera doesn't gain any height*/
+
+
+      float mainSpeed = 5.0f; //regular speed
+      float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+      float maxShift = 10.0f; //Maximum speed when holdin gshift
+      float camSens = 0.25f; //How sensitive it with mouse
+
+      private float offsetx = 0f;
+      private float offsety = 1.25f;
+      private float offsetz = -2.5f;
+
+      private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
+      private float totalRun= 1.0f;
+
+      GameObject m_MainCamera;
+
+      private void Awake() {
+          _realtimeView      = GetComponent<RealtimeView>();
+          _realtimeTransform = GetComponent<RealtimeTransform>();
+      }
+
+      private void Start(){
+          // InfoSync _InfoSync = GetComponent<InfoSync>();
+          // _InfoSync.SetName(PlayerPrefs.GetString("name"));
+      }
+
+
+      private bool isCameraParented = false;
+
+      private void Update() {
+          // If this CubePlayer prefab is not owned by this client, bail.
+          if (!_realtimeView.isOwnedLocally)
+              return;
+
+          // Move the camera
+          if (!isCameraParented) {
+              m_MainCamera = Camera.main.gameObject;
+              m_MainCamera.transform.parent = transform;
+              Vector3 offset = new Vector3(offsetx, offsety, offsetz);
+              m_MainCamera.transform.position = transform.position + offset;
+              m_MainCamera.transform.LookAt(transform);
+
+              isCameraParented = true;
+          }
+
+          // Make sure we own the transform so that RealtimeTransform knows to use this client's transform to synchronize remote clients.
+          _realtimeTransform.RequestOwnership();
+
+          // // Grab the x/y input from WASD / a controller
+          // float x = Input.GetAxis("Horizontal");
+          // float y = Input.GetAxis("Vertical");
+
+          // // Apply to the transform
+          // Vector3 localPosition = transform.localPosition;
+          // localPosition.x += x * speed * Time.deltaTime;
+          // localPosition.y += y * speed * Time.deltaTime;
+          // transform.localPosition = localPosition;
+
+          lastMouse = Input.mousePosition - lastMouse ;
+          lastMouse = new Vector3(-lastMouse.y * camSens, lastMouse.x * camSens, 0 );
+          lastMouse = new Vector3(transform.eulerAngles.x + lastMouse.x , transform.eulerAngles.y + lastMouse.y, 0);
+          transform.eulerAngles = lastMouse;
+          lastMouse =  Input.mousePosition;
+          //Mouse  camera angle done.
+
+          //Keyboard commands
+          Vector3 p = GetBaseInput();
+          if (Input.GetKey (KeyCode.LeftShift)){
+              totalRun += Time.deltaTime;
+              p  = p * totalRun * shiftAdd;
+              p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+              p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+              p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+          }
+          else{
+              totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+              p = p * mainSpeed;
+          }
+
+          p = p * Time.deltaTime;
+
+          Vector3 newPosition = transform.position;
+
+          if (Input.GetKey(KeyCode.Space)){ //If player wants to move on X and Z axis only
+              transform.Translate(p);
+              newPosition.x = transform.position.x;
+              newPosition.z = transform.position.z;
+              transform.position = newPosition;
+          }
+          else{
+              transform.Translate(p);
+          }
+
+      }
+
+      private Vector3 GetBaseInput() { //returns the basic values, if it's 0 than it's not active.
+          Vector3 p_Velocity = new Vector3();
+          if (Input.GetKey (KeyCode.W)){
+              p_Velocity += new Vector3(0, 0 , 1);
+          }
+          if (Input.GetKey (KeyCode.S)){
+              p_Velocity += new Vector3(0, 0, -1);
+          }
+          if (Input.GetKey (KeyCode.A)){
+              p_Velocity += new Vector3(-1, 0, 0);
+          }
+          if (Input.GetKey (KeyCode.D)){
+              p_Velocity += new Vector3(1, 0, 0);
+          }
+          return p_Velocity;
+      }
+    }
+}
