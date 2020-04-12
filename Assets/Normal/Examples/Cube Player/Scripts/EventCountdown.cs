4521fd5d3f0811d101c0c90274ad2951ea9cc2c5
alpertucanberk <alper.tu.canberk@gmail.com>
2020-04-12 14:45:56 -0700

    Update EventCountdown.cs

--- Assets/Normal/Examples/Cube Player/Scripts/EventCountdown.cs
+++ Assets/Normal/Examples/Cube Player/Scripts/EventCountdown.cs
@@ -7,77 +7,78 @@ using TMPro;
 using Normal.Realtime;
 using Michsky.UI.ModernUIPack;
 
-namespace Normal.Realtime.Examples {
-        public class EventCountdown : MonoBehaviour
+namespace Normal.Realtime.Examples
+{
+    public class EventCountdown : MonoBehaviour
+    {
+
+        private float currentTime = 0f;
+        public float countdownDuration;
+        public TMP_Text eventStartingNotificationDescription;
+        public string scene = "The Bowl";
+        private EventManager eventManager;
+        private GameObject localPlayer;
+
+
+
+        // Start is called before the first frame update
+        void Start()
         {
+            Debug.Log("Event countdown starting!!!!");
+            currentTime = countdownDuration;
+            eventManager = GameObject.Find("EventManager").GetComponent<EventManager>();
+            eventManager.OnEventsChange.AddListener(ReceiveEvent);
+            Debug.Log("Event manager from EventCountdown");
+            Debug.Log(eventManager);
+        }
 
-            private float currentTime = 0f;
-            public float countdownDuration;
-            public TMP_Text eventStartingNotificationDescription;
-            public string scene = "The Bowl";
-            private EventManager eventManager;
-            private GameObject localPlayer;
-
-
-
-            // Start is called before the first frame update
-            void Start()
+        void ReceiveEvent()
+        {
+            Debug.Log("events from ReceiveEvent");
+            Debug.Log(eventManager.events);
+            if (eventManager.events[0] == '0')
             {
-                Debug.Log("Event countdown starting!!!!");
-                currentTime = countdownDuration;
-                eventManager = GameObject.Find("EventManager").GetComponent<EventManager>();
-                eventManager.OnEventsChange.AddListener(ReceiveEvent);
-                Debug.Log("Event manager from EventCountdown");
-                Debug.Log(eventManager);
+                scene = "The Bowl";
             }
 
-            void ReceiveEvent()
+            countdownDuration = 10.0f;
+            if (eventManager.events[1] == '1')
             {
-                Debug.Log("events from ReceiveEvent");
-                Debug.Log(eventManager.events);
-                if (eventManager.events[0] == '0')
-                {
-                    scene = "The Bowl";
-                }
+                Debug.Log("Initializing countdown");
+            }
+            else
+            {
+                Debug.Log("Resetting countdown");
+                currentTime = countdownDuration;
+            }
 
-                countdownDuration = 10.0f;
-                if (eventManager.events[1] == '1')
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (eventManager.events[1] == '1')
+            {
+                if (currentTime > 0)
                 {
-                    Debug.Log("Initializing countdown");
+                    currentTime -= 1 * Time.deltaTime;
                 }
                 else
                 {
-                    Debug.Log("Resetting countdown");
-                    currentTime = countdownDuration;
-                }
-
-            }
-
-
-            // Update is called once per frame
-            void Update()
-            {
-                if (eventManager.events[1] == '1')
-                {
-                    if (currentTime > 0)
-                    {
-                        currentTime -= 1 * Time.deltaTime;
-                    }
-                    else
-                    {
                     //foreach (GameObject player in (GameObject.FindGameObjectsWithTag("Player"))) {
                     //    if (player.GetComponent<CubePlayer>().isLocallyOwned()) {
                     //    //Realtime.Destroy(player);
                     //    }
-                        GameObject.Find("Realtime").GetComponent<Realtime>().Disconnect();
-                    }
-                        Loading.sceneString = scene;
-                        SceneManager.LoadScene("Loading");
-                    }
-                    eventStartingNotificationDescription.text = "A virtual event is starting! You will automatically join the event in " + currentTime.ToString("0") + " seconds.";
+                    GameObject.Find("Realtime").GetComponent<Realtime>().Disconnect();
                 }
-
-
+                Loading.sceneString = scene;
+                SceneManager.LoadScene("Loading");
             }
+            eventStartingNotificationDescription.text = "A virtual event is starting! You will automatically join the event in " + currentTime.ToString("0") + " seconds.";
         }
+
+
     }
+}
+
