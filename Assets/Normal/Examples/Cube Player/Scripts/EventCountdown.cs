90f14dc6837f73500a8e8d699dba90cf9fd44946
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-12 01:55:31 -0400

    Event Scene Transfer

--- Assets/Normal/Examples/Cube Player/Scripts/EventCountdown.cs
+++ Assets/Normal/Examples/Cube Player/Scripts/EventCountdown.cs
@@ -2,25 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
-public class EventCountdown : MonoBehaviour
+namespace Michsky.UI.ModernUIPack
 {
-
-    private float currentTime = 0f;
-    public float countdownDuration = 60f;
-    public TMP_Text eventStartingNotificationDescription;
-
-    // Start is called before the first frame update
-    void Start()
+    public class EventCountdown : MonoBehaviour
     {
-        currentTime = countdownDuration;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(currentTime > 0) currentTime -= 1 * Time.deltaTime;
-        eventStartingNotificationDescription.text = "A virtual event is starting! You will automatically join the event in " + currentTime.ToString("0") + " seconds.";
+        private float currentTime = 0f;
+        public float countdownDuration = 60f;
+        public TMP_Text eventStartingNotificationDescription;
+        public string scene = "The Bowl";
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            currentTime = countdownDuration;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (currentTime > 0)
+            {
+                currentTime -= 1 * Time.deltaTime;
+            }
+            else
+            {
+                Loading.sceneString = scene;
+                SceneManager.LoadScene("Loading");
+            }
+
+            eventStartingNotificationDescription.text = "A virtual event is starting! You will automatically join the event in " + currentTime.ToString("0") + " seconds.";
+        }
     }
 }
