e71316a340cd54d9ec644e01292f2f6b1a570bf3
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-12 14:05:54 -0400

    Admin Panel

---
+++ Assets/Scenes/Admin Panel/AdminPanel.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AdminPanel : MonoBehaviour
+{
+    public TMP_Text buttonText;
+    public TMP_Text buttonHighlightedText;
+
+    private int _eventStatus = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeEventStatus(int eventIndex)
+    {
+        if(buttonText.text == "START")
+        {
+            buttonText.text = "END";
+            buttonHighlightedText.text = "CONFIRM";
+
+            _eventStatus = 1;
+        }
+        else
+        {
+            buttonText.text = "END";
+            buttonHighlightedText.text = "CONFIRM";
+
+            _eventStatus = 0;
+        }
+        
+    }
+}
