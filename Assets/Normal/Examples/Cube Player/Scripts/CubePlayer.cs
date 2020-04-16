ea22a8e693cc662c442ec6364a63a704f2810878
alpertucanberk <alper.tu.canberk@gmail.com>
2020-04-15 17:12:49 -0700

    podium camera setting up

--- Assets/Normal/Examples/Cube Player/Scripts/CubePlayer.cs
+++ Assets/Normal/Examples/Cube Player/Scripts/CubePlayer.cs
@@ -32,6 +32,7 @@ namespace Normal.Realtime.Examples
         private Vector3 lastMouse = new Vector3(255, 255, 255); //kind of in the middle of the screen, rather than at the top (play)
         private float totalRun = 1.0f;
 
+        public GameObject podiumCameraPosition;
         GameObject m_MainCamera;
         EventManager eventManager;
 
