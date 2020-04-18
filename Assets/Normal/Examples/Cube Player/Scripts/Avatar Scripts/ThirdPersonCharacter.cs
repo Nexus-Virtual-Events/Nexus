866ed0ea31b6099f0791a24be7de014e09a7f6f5
alpertucanberk <alper.tu.canberk@gmail.com>
2020-04-18 11:41:56 -0700

    natural movement?

--- Assets/Normal/Examples/Cube Player/Scripts/Avatar Scripts/ThirdPersonCharacter.cs
+++ Assets/Normal/Examples/Cube Player/Scripts/Avatar Scripts/ThirdPersonCharacter.cs
@@ -9,12 +9,12 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 	{
 		[SerializeField] float m_MovingTurnSpeed = 360;
 		[SerializeField] float m_StationaryTurnSpeed = 180;
-		[SerializeField] float m_JumpPower = 12f;
-		[Range(1f, 4f)][SerializeField] float m_GravityMultiplier = 2f;
-		[SerializeField] float m_RunCycleLegOffset = 0.2f; //specific to the character in sample assets, will need to be modified to work with others
+		float m_JumpPower = 8f;
+		float m_GravityMultiplier = 1f;
+		float m_RunCycleLegOffset = 0.1f; //specific to the character in sample assets, will need to be modified to work with others
 		[SerializeField] float m_MoveSpeedMultiplier = 1f;
-		[SerializeField] float m_AnimSpeedMultiplier = 1f;
-		float m_GroundCheckDistance = 0.5f;
+		float m_AnimSpeedMultiplier = 0.9f;
+		float m_GroundCheckDistance = 0.3f;
 
 		Rigidbody m_Rigidbody;
 		Animator m_Animator;
