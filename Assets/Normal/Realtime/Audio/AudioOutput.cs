a2f8da7a6bad91b8cedf6d5b444e6679dcda3a50
Ilter C <iltercanberk@gmail.com>
2020-04-18 12:59:19 -0700

    one liner

--- Assets/Normal/Realtime/Audio/AudioOutput.cs
+++ Assets/Normal/Realtime/Audio/AudioOutput.cs
@@ -98,7 +98,8 @@ namespace Normal.Realtime {
 
                         // TODO: If there's no spatializer, we need to do this, but if there is a spatializer, we can just copy the value.
                         // TODO: Why is the input signal we're getting not 1.0 when spatialization is turned off??
-                        data[sOut] = !_mute ? audioData[sIn] : 0.0f;
+                        // data[sOut] = !_mute ? audioData[sIn] : 0.0f;
+                        data[sOut] = !_mute ? data[sOut] * audioData[sIn] : 0.0f;
                     }
                 }
 
