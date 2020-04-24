19d60ea443773ad01be43e03d3d5a1a66f6c17ba
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-24 17:35:48 -0400

    Firebase Stuff

--- Assets/Scenes/Menus/Main Menu/MainMenu.cs
+++ Assets/Scenes/Menus/Main Menu/MainMenu.cs
@@ -7,16 +7,41 @@ using TMPro;
 
 namespace Michsky.UI.ModernUIPack
 {
-  public class MainMenu : MonoBehaviour
-  {
-      public TMP_InputField playerName;
-      public string scene;
+    public class MainMenu : MonoBehaviour
+    {
+        public TMP_InputField _email;
+        public TMP_InputField _password;
+        public Firebase.Auth.FirebaseUser user;
+        public string scene;
 
-      public void StartGame()
-      {
-        PlayerPrefs.SetString("playerName", playerName.text);
-        AvatarCreator.sceneString = scene;
-        SceneManager.LoadScene("Avatar Creator");
-      }
-  }
+        public void Login()
+        {
+            Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+            Firebase.Auth.Credential credential = Firebase.Auth.GoogleAuthProvider.GetCredential("299949821719-6f8dbg9jc00cscu1m34l0v2qutpsmvbm.apps.googleusercontent.com", "RwJoy_KuMfOv-LiR31_hCFPF");
+            auth.SignInWithCredentialAsync(credential).ContinueWith(task =>
+            {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithCredentialAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                Firebase.Auth.FirebaseUser user = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    user.DisplayName, user.UserId);
+            });
+        }
+
+        public void StartGame()
+        {
+            PlayerPrefs.SetString("playerName", user.DisplayName);
+            AvatarCreator.sceneString = scene;
+            SceneManager.LoadScene("Avatar Creator");
+        }
+    }
 }
