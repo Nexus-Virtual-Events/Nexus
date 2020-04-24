6635e0e81ff76d5c2b598671fa476fcaafa28508
Areeq Hasan <ahasan20@lawrenceville.org>
2020-04-24 18:47:35 -0400

    clothes

--- Assets/Scenes/Menus/Main Menu/MainMenu.cs
+++ Assets/Scenes/Menus/Main Menu/MainMenu.cs
@@ -14,31 +14,58 @@ namespace Michsky.UI.ModernUIPack
         public Firebase.Auth.FirebaseUser user;
         public string scene;
 
-        public void Login()
+        public void Register()
         {
             Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-            Firebase.Auth.Credential credential = Firebase.Auth.GoogleAuthProvider.GetCredential("299949821719-6f8dbg9jc00cscu1m34l0v2qutpsmvbm.apps.googleusercontent.com", "RwJoy_KuMfOv-LiR31_hCFPF");
-            auth.SignInWithCredentialAsync(credential).ContinueWith(task =>
+            auth.CreateUserWithEmailAndPasswordAsync(_email.text, _password.text).ContinueWith(task =>
             {
                 if (task.IsCanceled)
                 {
-                    Debug.LogError("SignInWithCredentialAsync was canceled.");
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
                     return;
                 }
                 if (task.IsFaulted)
                 {
-                    Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                    Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
                     return;
                 }
 
-                Firebase.Auth.FirebaseUser user = task.Result;
+                // Firebase user has been created.
+                user = task.Result;
+                Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                    user.DisplayName, user.UserId);
+
+                StartGame();
+            });
+        }
+
+        public void Login()
+        {
+            Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+            auth.SignInWithEmailAndPasswordAsync(_email.text, _password.text).ContinueWith(task =>
+            {
+                if (task.IsCanceled)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                    return;
+                }
+
+                user = task.Result;
                 Debug.LogFormat("User signed in successfully: {0} ({1})",
                     user.DisplayName, user.UserId);
+
+                StartGame();
             });
         }
 
         public void StartGame()
         {
+            Debug.Log("StartGame() Called!");
             PlayerPrefs.SetString("playerName", user.DisplayName);
             AvatarCreator.sceneString = scene;
             SceneManager.LoadScene("Avatar Creator");
