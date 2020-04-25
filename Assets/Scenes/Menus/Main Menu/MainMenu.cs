b10b770537bda54da557aae0a0a26b05c82ae36a
alpertucanberk <alper.tu.canberk@gmail.com>
2020-04-25 13:37:54 -0700

    login completed

--- Assets/Scenes/Menus/Main Menu/MainMenu.cs
+++ Assets/Scenes/Menus/Main Menu/MainMenu.cs
@@ -10,20 +10,31 @@ using System.Text;
 
 namespace Michsky.UI.ModernUIPack
 {
+
+    public class LoginInfo
+    {
+        public int code;
+        public string admin;
+        public string name;
+
+    }
+
     public class MainMenu : MonoBehaviour
     {
         public TMP_InputField _email;
         public TMP_InputField _password;
-        public TMP_Text _warningText;
+        public Text _warningText;
 
         public string scene;
         public string displayName;
         private string key = "NexusConnects";
 
-        private void Start()
-        {
-            _warningText.text = "";
-        }
+        string url = "http://127.0.0.1:5000/authenticate_with_unity";
+
+        //private void Start()
+        //{
+        //    _warningText.text = "";
+        //}
 
         IEnumerator Post(string url, string bodyJsonString)
         {
@@ -39,19 +50,80 @@ namespace Michsky.UI.ModernUIPack
 
         public void Login()
         {
-            //StartGame();
-            Debug.Log("login activated with " + _email.text + _password.text);
-            string jsonString = "{\"email\":" + _email.text + ",\"password\":" + _password.text + ",\"key\":" + key + "}";
-            //CoroutineWithData cd = new CoroutineWithData(this, Post("http://the-nexus.herokuapp.com/authenticate_with_unity", jsonString));
-            //yield return cd.coroutine;
-            StartCoroutine(Post("http://the-nexus.herokuapp.com/authenticate_with_unity", jsonString));
-            //Debug.Log("result is " + cd.result);  //  'success' or 'fail'
+            StartCoroutine(SendPostCoroutine());
         }
 
-        public void StartGame()
+        IEnumerator SendPostCoroutine()
+        {
+            //StartGame();
+            //Debug.Log("login activated with " + _email.text + _password.text);
+            //string jsonString = "{\"email\":\"" + _email.text + "\",\"password\":\"" + _password.text + "\",\"key\":\"" + key + "\"}";
+            //Debug.Log(jsonString);
+            ////CoroutineWithData cd = new CoroutineWithData(this, Post("http://127.0.0.1:5000/authenticate_with_unity", jsonString));
+            ////yield return cd.coroutine;
+            ////StartCoroutine(Post("http://127.0.0.1:5000/authenticate_with_unity", jsonString));
+            ////Debug.Log("result is " + cd.result);  //  'success' or 'fail'
+            ///
+
+            WWWForm form = new WWWForm();
+            form.AddField("email", _email.text);
+            form.AddField("password", _password.text);
+            form.AddField("key", key);
+
+            using (UnityWebRequest www = UnityWebRequest.Post(url, form))
+            {
+                yield return www.SendWebRequest();
+
+                if (www.isNetworkError)
+                {
+                    Debug.Log(www.error);
+                }
+                else
+                {
+                    Debug.Log("POST successful!");
+                    StringBuilder sb = new StringBuilder();
+                    foreach (System.Collections.Generic.KeyValuePair<string, string> dict in www.GetResponseHeaders())
+                    {
+                        sb.Append(dict.Key).Append(": \t[").Append(dict.Value).Append("]\n");
+                    }
+
+                    // Print Headers
+                    Debug.Log(sb.ToString());
+
+                    // Print Body
+                    Debug.Log(www.downloadHandler.text);
+                    LoginInfo info = JsonUtility.FromJson<LoginInfo>(www.downloadHandler.text);
+                    Debug.Log(info.code.GetType());
+                    Debug.Log(info.code);
+                    if(info.code == 0)
+                    {
+                        Debug.Log("Success!");
+                        Debug.Log(info.name);
+                        Debug.Log(info.admin);
+                        StartGame(info.name, info.admin);
+                    }
+                    else if (info.code == 403)
+                    {
+                        Debug.Log("wrong pass");
+                        _warningText.text = "Are you sure you put in the right password? Check the website.";
+                    }
+                    else if(info.code == 404)
+                    {
+                        Debug.Log("not found");
+                        _warningText.text = "User not found, make sure you put in the right email";
+                    }
+                }
+            }
+
+
+        }
+
+        public void StartGame(string name, string isAdmin)
         {
             Debug.Log("StartGame() Called!");
-            PlayerPrefs.SetString("playerName", displayName);
+            PlayerPrefs.SetString("playerName", name);
+            PlayerPrefs.SetString("isAdmin", isAdmin);
+
             AvatarCreator.sceneString = scene;
             SceneManager.LoadScene("Avatar Creator");
         }
