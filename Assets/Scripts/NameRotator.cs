bf2f3ab1c1083294a4bbe9ad9970dd3ab93e3d49
Areeq Hasan <ahasan20@lawrenceville.org>
2020-05-02 12:33:27 -0400

    mustace

---
+++ Assets/Scripts/NameRotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameRotator : MonoBehaviour
+{
+    public float offsetx;
+    public float offsety;
+    public float offsetz;
+
+    public Vector3 offsetcamera;
+
+
+    private Vector3 parentOffset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //parentOffset = transform.position - transform.parent.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = transform.parent.position;
+        //transform.LookAt(2 * transform.position - (Camera.main.transform.position+offsetcamera));
+        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward,
+Camera.main.transform.rotation * Vector3.up);
+    }
+}
