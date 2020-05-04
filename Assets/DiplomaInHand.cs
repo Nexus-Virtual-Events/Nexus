e3de9f2de43e79777bc7282ba0d427e6c8b45186
alpertucanberk <alper.tu.canberk@gmail.com>
2020-05-03 18:00:27 -0700

    ignore this

--- Assets/DiplomaInHand.cs
+++ Assets/DiplomaInHand.cs
@@ -9,32 +9,34 @@ public class DiplomaInHand : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
         ActionRouter.GetLocalAvatar().GetComponent<DynamicCharacterAvatar>().SetSlot("Hands", "Diploma_Recipe");
         ActionRouter.GetLocalAvatar().GetComponent<DynamicCharacterAvatar>().BuildCharacter();
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+    }
 
     //OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    ActionRouter.GetLocalAvatar().GetComponent<DynamicCharacterAvatar>().SetSlot("Hands", "None");
-    //    ActionRouter.GetLocalAvatar().GetComponent<DynamicCharacterAvatar>().BuildCharacter();
-    //}
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+    }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
+    override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateMove(animator, stateInfo, layerIndex);
+    }
 
     // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+    override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateIK(animator, stateInfo, layerIndex);
+    }
 }
