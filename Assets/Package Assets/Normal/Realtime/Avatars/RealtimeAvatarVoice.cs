9320e6da7b8c3b53813ed70f6feeaca1c2489323
Areeq Hasan <ahasan20@lawrenceville.org>
2020-05-10 02:56:02 -0400

    realtime vpice

--- Assets/Package Assets/Normal/Realtime/Avatars/RealtimeAvatarVoice.cs
+++ Assets/Package Assets/Normal/Realtime/Avatars/RealtimeAvatarVoice.cs
@@ -172,7 +172,8 @@ namespace Normal.Realtime {
                 
                 if (createAudioPreprocessor) {
                     // Turn on echo cancellation for mobile devices;
-                    bool echoCancellation = Application.isMobilePlatform && Application.platform != RuntimePlatform.IPhonePlayer;
+                    //bool echoCancellation = Application.isMobilePlatform && Application.platform != RuntimePlatform.IPhonePlayer;
+                    bool echoCancellation = true;
                     _audioPreprocessor = new AudioPreprocessor(_microphoneSampleRate,_microphoneFrameSize,                  // Input stream
                                                                true,                                                        // Automatic gain control
                                                                true,                                                        // Noise suppression
