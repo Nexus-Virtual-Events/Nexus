804b29e0bc7ee001c7f4f7c5c3e1516e34c92764
Areeq Hasan <ahasan20@lawrenceville.org>
2020-05-13 00:28:49 -0400

    Revert "diploma stuff almost done"
    This reverts commit 983bc8453d3d76f1367e065abb2b974ad9dfa50e.

--- Assets/Scripts/AvatarScripts/ThirdPersonUserControl.cs
+++ Assets/Scripts/AvatarScripts/ThirdPersonUserControl.cs
@@ -83,14 +83,19 @@ public class ThirdPersonUserControl : MultiplayerMonoBehavior
     public void GetDiploma(){
         System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
         int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
-        GetComponent<StateSync>().SetState("0_1_0_"+cur_time.ToString());
-            
+        if(!diplomaGotten){
+            GetComponent<StateSync>().SetState("0_1_0_"+cur_time.ToString());
+            diplomaGotten = true;
+        }
     }
 
     public void GiveDiploma(){
         System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
         int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalSeconds;
-        GetComponent<StateSync>().SetState("0_0_0_"+cur_time.ToString());
+        if(diplomaGotten){
+            GetComponent<StateSync>().SetState("0_0_0_"+cur_time.ToString());
+            diplomaGotten = false;
+        }
     }
 
     private bool firstAnimationOfInitial = true;
@@ -118,7 +123,6 @@ public class ThirdPersonUserControl : MultiplayerMonoBehavior
         }
         else{
             if(firstAnimationOfMiddle){
-                GetDiploma();
                 // GiveDiploma();  
             }
             if(!firstAnimationOfMiddle){
