be04d3402fa949560be925ec35b73adcdc5d4fe5
alpertucanberk <alper.tu.canberk@gmail.com>
2020-05-16 17:49:55 -0700

    more

--- Assets/Scripts/AvatarScripts/ThirdPersonUserControl.cs
+++ Assets/Scripts/AvatarScripts/ThirdPersonUserControl.cs
@@ -214,7 +214,7 @@ public class ThirdPersonUserControl : MultiplayerMonoBehavior
     {
         string[] actionParts = lastAction.Split('_');
 
-        Debug.Log(">> Starting action remotely from "+ gameObject.name);
+        Debug.Log(">> Starting action from "+ gameObject.name);
         Debug.Log(">> animation is local? " + isAnimationLocal.ToString() + " " + _realtimeView.ownerID);
         m_Character.StartAnimation(actionParts[0], isAnimationLocal);
 
@@ -536,8 +536,6 @@ public class ThirdPersonUserControl : MultiplayerMonoBehavior
 
         // Update Move Here
         GetComponent<MoveSync>().SetLastAction(currentInteraction + "_" + cur_time.ToString());
-        Debug.Log(">> starting animation locally from " + gameObject.name);
-        Debug.Log(">> animation is local? " + isAnimationLocal.ToString() + " " + _realtimeView.ownerID);
         // m_Character.StartAnimation(currentInteraction, isAnimationLocal);
 
         canMove = true;
