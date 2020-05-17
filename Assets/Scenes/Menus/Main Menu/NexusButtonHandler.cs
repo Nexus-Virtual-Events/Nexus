98f2cc97b0e2dce8ea5d6cef8ea3c296dd0d49f2
Areeq Hasan <ahasan20@lawrenceville.org>
2020-05-16 23:56:42 -0400

    scenes

---
+++ Assets/Scenes/Menus/Main Menu/NexusButtonHandler.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class NexusButtonHandler : MonoBehaviour
+{
+
+    /// <summary>
+    ///   React to a button click event.  Used in the UI Button action definition.
+    /// </summary>
+    /// <param name="button"></param>
+    public void onButtonClicked(Button button)
+    {
+        // which GameObject?
+        GameObject go = GameObject.Find("GameController");
+        if (go != null)
+        {
+            AgoraMainMenu gameController = go.GetComponent<AgoraMainMenu>();
+            if (gameController == null)
+            {
+                Debug.LogError("Missing game controller...");
+                return;
+            }
+            if (button.name == "JoinButton")
+            {
+                gameController.onJoinButtonClicked();
+            }
+            else if (button.name == "LeaveButton")
+            {
+                gameController.onLeaveButtonClicked();
+            }
+        }
+    }
+}
