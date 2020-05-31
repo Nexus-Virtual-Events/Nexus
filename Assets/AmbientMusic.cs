812c78d463b36f6acf8a082a298a66f87dc0e340
alpertucanberk <alper.tu.canberk@gmail.com>
2020-05-31 01:15:05 -0700

    holy moly this map looks amazing

---
+++ Assets/AmbientMusic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientMusic : MonoBehaviour
+{
+    AudioSource fxSound;
+    public AudioClip backMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fxSound = GetComponent<AudioSource>();   
+        Invoke("PlayMusic", 5.0f);
+    }
+
+    void PlayMusic(){
+        fxSound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
